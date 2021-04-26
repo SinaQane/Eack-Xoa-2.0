ae947d252ac9aec680748f7928e4e32c251c8705
Sina Ghaseminejad <sina.qane@yahoo.com>
2021-04-27 00:09:40 +0430

    Changed packaging

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,11 +1,6 @@
+import apps.authentication.view.FXMLController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.util.Objects;
-
 
 public class MainApp extends Application
 {
@@ -13,17 +8,14 @@ public class MainApp extends Application
     @Override
     public void start(Stage stage) throws Exception
     {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene.fxml")));
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
-
+        FXMLController fxmlController = new FXMLController();
         stage.setTitle("JavaFX and Gradle");
-        stage.setScene(scene);
+        stage.setScene(fxmlController.getScene());
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 
