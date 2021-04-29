a5f3387dce1309e5fb97f7441d57f09828eac3ec
Sina Ghaseminejad <sina.qane@yahoo.com>
2021-04-29 17:29:49 +0430

    Added LoginPage and SignUpPage

--- src/main/java/view/MainStage.java
+++ src/main/java/view/MainStage.java
@@ -1,6 +1,8 @@
 package view;
 
+import apps.authentication.view.LoginFXML;
 import apps.authentication.view.LoginPage;
+import apps.authentication.view.SignUpFXML;
 import apps.authentication.view.SignUpPage;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -11,13 +13,12 @@ import java.io.IOException;
 
 public class MainStage
 {
-    private final FirstPage firstPage = new FirstPage();
+    private final FirstPage firstPage = new FirstPage(); // TODO After logout, user should be redirected here.
     private final LoginPage loginPage = new LoginPage();
     private final SignUpPage signUpPage = new SignUpPage();
 
     public MainStage(Stage stage) throws IOException
     {
-        stage.setTitle("Eack Xoa");
         FXMLLoader firstPageLoader = firstPage.getLoader();
         FirstPageFXML firstPageController = firstPageLoader.getController();
         firstPageController.setListener(string -> {
@@ -30,6 +31,34 @@ public class MainStage
                 stage.setScene(signUpPage.getScene());
             }
         });
+
+        FXMLLoader loginPageLoader = loginPage.getLoader();
+        LoginFXML loginPageController = loginPageLoader.getController();
+        loginPageController.setListener(string -> {
+            if (string.equals("Enter"))
+            {
+                System.out.println("Enter");
+            }
+            else if (string.equals("SignUp"))
+            {
+                stage.setScene(signUpPage.getScene());
+            }
+        });
+
+        FXMLLoader signUpPageLoader = signUpPage.getLoader();
+        SignUpFXML signUpPageController = signUpPageLoader.getController();
+        signUpPageController.setListener(string -> {
+            if (string.equals("SignUp"))
+            {
+                System.out.println("SignUp");
+            }
+            else if (string.equals("Login"))
+            {
+                stage.setScene(loginPage.getScene());
+            }
+        });
+
+        stage.setTitle("Eack Xoa");
         stage.setScene(firstPage.getScene());
         stage.show();
     }
